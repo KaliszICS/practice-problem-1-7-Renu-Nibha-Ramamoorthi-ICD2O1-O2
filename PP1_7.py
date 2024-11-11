5259e26bc8730561e911206837df0c557eba8fbd
Renu-Nibha-Ramamoorthi-ICD2O1-O2 <renunibha.ramamoorthi@student.tdsb.on.ca>
2024-11-11 15:02:31 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -2,19 +2,26 @@
 
 def q1():
   #Write code here
-
+  print(True) 
 def q2():
   #Write code here
-
+  num = int(input("Input an integer: ")) 
+  print(num > 5) 
 def q3():
   #Write code here
-
+  letter = input("Input the letter a: ") 
+  print(letter == "a")
 def q4():
   #Write code here
-
+  word = input("Input a word earlier in the dictionary than google: ") 
+  print(word < "google") 
+  
 def q5():
   #Write code here
-
+  num1 = int(input("Input an integer: ")) 
+  num2 = int(input("Input another integer: ")) 
+  result = num1 * num2 > 40 
+  print("Your numbers multiplied together are greater than 40:", result) 
 #Do edit the code below
 #Comment the lines below when running your tests
 
